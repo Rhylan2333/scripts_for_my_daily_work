d67ba65527e606440a53260c527910f871f262e9
Rhylan Cai <75231089+Rhylan2333@users.noreply.github.com>
2026-01-05 17:10:06 +0800

    Update ena_fastq_ascp_download.sh

--- ena_fastq_ascp_download.sh
+++ ena_fastq_ascp_download.sh
@@ -1,5 +1,6 @@
-# SraRunTable.[your PRJNAXXXX ID].csv: Download Total-Metadata from https://trace.ncbi.nlm.nih.gov/Traces/study/?acc=[your PRJNAXXXX ID]&o=acc_s%3Aa. Get SraRunTable.csv, then rename.
+# SraRunTable. [your PRJNAXXXX ID].csv: Download Total-Metadata from https://trace.ncbi.nlm.nih.gov/Traces/study/?acc=[your PRJNAXXXX ID]&o=acc_s%3Aa. Get SraRunTable.csv, then rename.
 # filereport_read_run_[your PRJNAXXXX ID].tsv: Choose "run_accession", "fastq_bytes", "fastq_md5", "fastq_aspera" cols, download TSV report from https://www.ebi.ac.uk/ena/browser/view/[your PRJNAXXXX ID]
+
 
 awk 'BEGIN{FS=OFS="\t"} {
   split($2, arr2, ";")
@@ -10,16 +11,21 @@ awk 'BEGIN{FS=OFS="\t"} {
   }
 }' < filereport_read_run_[your PRJNAXXXX ID].tsv > filereport_read_run_[your PRJNAXXXX ID].not_merged.tsv
 
+
 mkdir [your PRJNAXXXX ID]-ascp/
 sd 'fasp.+/' '' < filereport_read_run_[your PRJNAXXXX ID].not_merged.tsv | csvtk -t cut -f 'fastq_md5,fastq_aspera' -U -D ' ' | sort -k 2  > [your PRJNAXXXX ID]-ascp/fastq_md5.should_be.md5
+
 
 csvtk -t cut -f 'fastq_aspera' filereport_read_run_[your PRJNAXXXX ID].not_merged.tsv -U | sd '^fasp' 'ascp -v -QT -P 33001 -k 1 -l 200m -i /home/caicai/.local/share/mamba/envs/aspera-cli/etc/asperaweb_id_dsa.openssh "era-fasp@fasp' | sd '.gz$' '.gz" ./[your PRJNAXXXX ID]-ascp/' > filereport_read_run_[your PRJNAXXXX ID].not_merged.ascp.sh
 chmod +x filereport_read_run_[your PRJNAXXXX ID].not_merged.ascp.sh
 
+
 ./filereport_read_run_[your PRJNAXXXX ID].not_merged.ascp.sh
+
 
 z [your PRJNAXXXX ID]-ascp/
 script -c 'md5sum -c fastq_md5.should_be.md5' md5sum-check.log
+
 
 z ..
 script -c '''
@@ -30,6 +36,10 @@ script -c '''
   done
 ''' ./log.mv_fastq.log
 
+
 seqkit stats $(fd '.fastq.gz' [your PRJNAXXXX ID]-ascp/) -j 14 -aT -o [your PRJNAXXXX ID]-ascp/seqkit-stats.tsv
 
+
 # Then，do BWA ...
+
+# "java: symbol lookup error: java: undefined symbol: JLI_StringDup" May due to java-jdk in environment, mamba remove it, then remove and install gatk4.
